e13a29f46f3f135ad04048c9e5bfd0fe71d2f5f0
Alexander K <scor2k@gmail.com>
2020-03-17 22:56:26 +0300

    Added digital training

--- ielts_listening/ielts_listening.py
+++ ielts_listening/ielts_listening.py
@@ -3,17 +3,41 @@
 
 import click
 from datetime import datetime
+from google_speech import Speech
+from random import randrange
+from time import sleep
 
 @click.group()
 def cli():
     """IELTS Listening Training"""
 
-@cli.command(name="test")
-def test():
-    """Show test and exit."""
+@cli.command(name="digits")
+@click.option("--amount", type=int, help="How many numbers you wanna check", required=True)
+@click.option("--max", help="The max number you wanna listen.", default=2500 )
+def test_digits(amount, max):
+    """Show and check digits."""
 
-    print ("Hello world!")
+    lang = "en"
+    result = 0
+
+    for idx in range(amount) :
+        sleep(0.5)
+        print ("~"*10)
+        rnd = randrange(0, max)
+        text = f"{rnd}"
+        Speech(text, lang).play()
+
+        test = input("Your answer: ")
+
+        if test == text :
+            result += 1
+            print ("You are right.")
+        else :
+            print (f"The right answer is: {rnd}")
     
+    print ("Result:")
+    print (f" {amount} - the number of questions")
+    print (f" {result} - the number of RIGHT answers")
 
 
 if __name__ == "__main__":
